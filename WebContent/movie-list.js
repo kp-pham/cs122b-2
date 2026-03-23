8d6256eee180101a6ab00d70de03e1ef33910264
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-23 15:47:38 -0700

    refactor: hyperlinks for movies and stars

--- WebContent/movie-list.js
+++ WebContent/movie-list.js
@@ -4,11 +4,17 @@ function handleResult(resultData) {
     resultData.forEach(movie => {
         let row = `
             <tr>
-                <td>${movie['title']}</td>
+                <td>
+                    <a href="single-movie.html?id=${movie['id']}">${movie['title']}</a>
+                </td>
                 <td>${movie['year']}</td>
                 <td>${movie['director']}</td>
                 <td>${movie['genres'].slice(0, 3).join(', ')}</td>
-                <td>${movie['stars'].slice(0, 3).join(', ')}</td>
+                <td>
+                    ${movie['stars'].slice(0, 3).map(({id, name}) => {
+                        return `<a href="single-star.html?id=${id}">${name}</a>`
+                    }).join(', ')}
+                </td>
                 <td>${movie['rating']}</td>
             </tr>
         `;
