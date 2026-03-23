d0387b066be6ed5c02a9ad4ba6057eea684efb84
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-22 20:45:29 -0700

    chore: import JSONParser from GSON library

--- src/SingleMovieServlet.java
+++ src/SingleMovieServlet.java
@@ -1,5 +1,6 @@
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
