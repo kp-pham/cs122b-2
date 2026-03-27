0ecc7144b972d58533bbc94760d49aaf28db4eec
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-27 16:52:46 -0700

    feat: reuse query to benefit from existing indexes

--- src/SearchServlet.java
+++ src/SearchServlet.java
@@ -34,15 +34,30 @@ public class SearchServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
 
-        String title = request.getParameter("title");
-        String year = request.getParameter("year");
-        String director = request.getParameter("director");
-        String star = request.getParameter("star");
+        String title = request.getParameter("title").trim();
+        String year = request.getParameter("year").trim();
+        String director = request.getParameter("director").trim();
+        String star = request.getParameter("star").trim();
 
         PrintWriter out = response.getWriter();
 
         try (Connection conn = dataSource.getConnection()) {
-
+            String query = "SELECT M.id, M.title, M.year, M.director, M.rating, " +
+                           "CONCAT('[', GROUP_CONCAT(DISTINCT G.name SEPARATOR ', '), ']') AS genres, " +
+                           "CONCAT('[', GROUP_CONCAT(DISTINCT JSON_OBJECT('id', S.id, 'name', S.name)), ']') AS stars " +
+                           "FROM ( " +
+                           "    SELECT M.id, M.title, M.year, M.director, R.rating " +
+                           "    FROM movies AS M " +
+                           "    LEFT JOIN ratings R ON M.id = R.movieId " +
+                           "    ORDER BY R.rating DESC " +
+                           "    LIMIT 20" +
+                           ") AS M " +
+                           "LEFT JOIN genres_in_movies AS GIM ON M.id = GIM.movieId " +
+                           "LEFT JOIN genres AS G ON GIM.genreId = G.id " +
+                           "LEFT JOIN stars_in_movies AS SIM ON M.id = SIM.movieId " +
+                           "LEFT JOIN stars AS S ON SIM.starId = S.id " +
+                           "GROUP BY M.id, M.title, M.year, M.director, M.rating " +
+                           "ORDER BY M.rating DESC"
 
         } catch (Exception e) {
             JsonObject jsonObject = new JsonObject();
