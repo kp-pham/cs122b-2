3372d9b726d33a8f861f8a4c08bb73bd7650f7ee
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-28 16:38:43 -0700

    feat: show results from browsing servlet

--- WebContent/scripts/browse.js
+++ WebContent/scripts/browse.js
@@ -16,6 +16,27 @@ function getParameterByName(target) {
     return decodeURIComponent(results[2]);
 }
 
+function handleResult(resultData) {
+    $('title').text(resultData["name"]);
+    $('h3').text(resultData["name"] + ' (' + (resultData["birthYear"] ?? "N/A") + ')');
+
+    let movieTable = jQuery("#movie_table_body");
+
+    resultData["movies"].forEach(movie => {
+        let row = `
+            <tr>
+                <td>
+                    <a href="single-movie.html?id=${movie['id']}">${movie["title"]}</a>
+                </td>
+                <td>${movie["year"]}</td>
+                <td>${movie["director"]}</td>
+            </tr>
+        `;
+
+        movieTable.append(row);
+    });
+}
+
 function buildUrl() {
     let genre = getParameterByName("genre");
     let prefix = getParameterByName("prefix");
