64c5a3352ccf98fa5ebaf6c0924d65bb21238b09
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-29 20:11:33 -0700

    feat: show shopping cart

--- WebContent/scripts/cart.js
+++ WebContent/scripts/cart.js
@@ -1,3 +1,29 @@
+function handleResult(resultData) {
+    let itemsTable = $("#items-table");
+
+    resultData["items"].forEach(item => {
+        let row = `
+            <tr>
+                <td>
+                    <a href="single-movie.html?id=${item['id']}">${item['title']}</a>
+                </td>
+                <td>
+                   <button>-</button>
+                   ${item['quantity']}
+                   <button>+</button>
+                </td>
+                <td>
+                    <button>Remove</button>
+                </td>
+                <td>${item['price']}</td>
+                <td>${item['subtotal']}</td>
+            </tr>
+        `;
+
+        itemsTable.append(row);
+    });
+}
+
 jQuery.ajax({
     dataType: "json",
     method: "GET",
